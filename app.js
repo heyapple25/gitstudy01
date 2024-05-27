eab879878e5006952c5f924557a7d0a2ad0a4cc6
heyapple25 <hyub1509@naver.com>
2024-05-27 14:39:59 +0900

    home and root

---
+++ app.js
@@ -0,0 +1,15 @@
+const http=require("http");
+const express=require('express');
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.end("<h3>hello </h3>");
+})
+app.get('/home',(req,res)=>{
+    res.end("<h3>home </h3>");
+})
+
+const server=http.createServer(app);
+server.listen(3005,()=>{
+    console.log('Run on Server port : 3005')
+})
